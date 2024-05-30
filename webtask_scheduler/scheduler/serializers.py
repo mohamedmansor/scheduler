be4b83dd0a7b9a2fd6c98cffe0ac12dc02d83544
Mohamed Mansor <mohamedmansor212@gmail.com>
2024-05-30 06:43:10 +0300

    Add TestSetTimerAPIView

--- webtask_scheduler/scheduler/serializers.py
+++ webtask_scheduler/scheduler/serializers.py
@@ -2,9 +2,9 @@ from rest_framework import serializers
 
 
 class SetTimerInputSerializer(serializers.Serializer):
-    hours = serializers.IntegerField(help_text="Number of hours to wait before sending the request.")
-    minutes = serializers.IntegerField(help_text="Number of minutes to wait before sending the request.")
-    seconds = serializers.IntegerField(help_text="Number of seconds to wait before sending the request.")
+    hours = serializers.IntegerField(help_text="Number of hours to wait before sending the request.", min_value=0)
+    minutes = serializers.IntegerField(help_text="Number of minutes to wait before sending the request.", min_value=0)
+    seconds = serializers.IntegerField(help_text="Number of seconds to wait before sending the request.", min_value=0)
     web_url = serializers.URLField(help_text="URL to send the request to.")
 
     class Meta:
--- webtask_scheduler/scheduler/tests/test_views.py
+++ webtask_scheduler/scheduler/tests/test_views.py
@@ -59,4 +59,56 @@ class TestGetTimerAPIView:
         assert response.json() == {"task_id": test_data.id, "time_left_in_seconds": int(time_left)}
 
 
-# TODO Add tests for SetTimerAPIView
+class TestSetTimerAPIView:
+    @pytest.fixture
+    def api_client(self) -> APIClient:
+        return APIClient()
+
+    def test_set_timer_negative_numbers_validation(self, api_client):
+        url = reverse("api:scheduler:set-timer")
+        payload = {
+            "hours": -1,
+            "minutes": -1,
+            "seconds": -1,
+            "web_url": "https://example.com",
+        }
+        response = api_client.post(url, payload)
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.json() == {
+            "hours": ["Ensure this value is greater than or equal to 0."],
+            "minutes": ["Ensure this value is greater than or equal to 0."],
+            "seconds": ["Ensure this value is greater than or equal to 0."],
+        }
+
+    def test_set_timer_url_validation(self, api_client):
+        url = reverse("api:scheduler:set-timer")
+        payload = {
+            "hours": 1,
+            "minutes": 0,
+            "seconds": 0,
+            "web_url": "invalid url",
+        }
+        response = api_client.post(url, payload)
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.json() == {"web_url": ["Enter a valid URL."]}
+
+    @time_machine.travel(dt.datetime(2024, 5, 31, 1, 24, tzinfo=pytz.utc))
+    def test_set_timer_create_task_successfully(self, api_client):
+        url = reverse("api:scheduler:set-timer")
+        payload = {
+            "hours": 1,
+            "minutes": 0,
+            "seconds": 0,
+            "web_url": "https://example.com",
+        }
+
+        response = api_client.post(url, payload)
+        assert response.status_code == status.HTTP_201_CREATED
+
+        task = PeriodicTask.objects.last()
+        time_left = round((task.clocked.clocked_time - timezone.now().replace(tzinfo=pytz.utc)).total_seconds(), 1)
+        assert response.json() == {"task_id": task.id, "time_left_in_seconds": int(time_left)}
+        assert task.name == f"Get request to https://example.com at {task.clocked.clocked_time}"
+        assert task.task == "webtask_scheduler.scheduler.tasks.send_request_to_url"
+        assert task.args == '["https://example.com"]'
+        assert task.one_off is True
